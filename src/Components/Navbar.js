031adf010ff8dfd6b01baed143ee83685a794ebd
vikneshwaranvikey <vikeyviknesh@gmail.com>
2023-08-23 00:20:22 +0530

    Router-Scroll

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 const Navbar = () => {
   return (
-    <div className="header">
-      <div className="logo">
-        <h1>ROUTER-DOM</h1>
+    <>
+      <div className="header">
+        <div className="logo">
+          <h1>ROUTER-SCROLL</h1>
+        </div>
+        <nav className="navigator">
+          <ul>
+            <li>
+              <HashLink smooth to="/#home">
+                Home
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#about">
+                About
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#contact">
+                Contact Us
+              </HashLink>
+            </li>
+            <li>
+              <HashLink smooth to="/#course">
+                Developes
+              </HashLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="buttons">
+          <button type="button">Login</button>
+        </div>
       </div>
-      <nav className="navigator">
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
-          <li>
-            <Link to="/course">Developes</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="buttons">
-        <button type="button">Login</button>
-      </div>
-    </div>
+    </>
   );
 };
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Course from "./Components/Course";
 import Contact from "./Components/Contact";
-import { BrowserRouter, Route } from "react-router-dom";
+
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
@@ -14,10 +15,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Route path="/home" component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/course" component={Course}></Route>
-      <Route path="/contact" component={Contact}></Route>
+      <Home />
+      <About />
+      <Contact />
+      <Course />
     </BrowserRouter>
   );
 };
